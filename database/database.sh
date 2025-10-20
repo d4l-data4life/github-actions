d7da895c6393dca13303acddcf6be98ced51af3e
Christian Hentschel <christian.hentschel@data4life.care>
2025-10-20 15:28:33 +0200

    adds missing '&&'

--- database/database.sh
+++ database/database.sh
@@ -41,7 +41,7 @@ fi
 if [ "${OPERATION}" = "create" ]; then
     PGPASSWORD="${SU_PASSWORD}" psql -U "${SU_NAME}" -c "CREATE ROLE \"${USERNAME}\" CREATEDB LOGIN PASSWORD '${PASSWORD}';" && \
         PGPASSWORD="${PASSWORD}" psql -U "${USERNAME}" -c "CREATE DATABASE \"${DBNAME}\";" && \
-        PGPASSWORD="${PASSWORD}" psql -U "${USERNAME}" -d "${DBNAME}" -c "CREATE SCHEMA \"${DBSCHEMA}\";"
+        PGPASSWORD="${PASSWORD}" psql -U "${USERNAME}" -d "${DBNAME}" -c "CREATE SCHEMA \"${DBSCHEMA}\";" && \
         PGPASSWORD="${SU_PASSWORD}" psql -U "${SU_NAME}" -d "${DBNAME}" -c "GRANT USAGE ON SCHEMA \"${DBSCHEMA}\" TO \"${USERNAME}\";" && \
         PGPASSWORD="${SU_PASSWORD}" psql -U "${SU_NAME}" -d "${DBNAME}" -c "GRANT CREATE ON SCHEMA \"${DBSCHEMA}\" TO \"${USERNAME}\";"
     # return early with the last seen exit code - otherwise the next if [...] would return 0
